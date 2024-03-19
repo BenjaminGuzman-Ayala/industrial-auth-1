5ff7e45e49bb68e6829b7bffb606c84ff1104fd8
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-03-19 21:42:12 +0000

    added pundit policies

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
-  # before_action :is_an_authorized_user, only: [:create]
-
+before_action {authorize (authorize(@comment || Comment))}
 
   # GET /comments or /comments.json
   def index
@@ -52,6 +51,7 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1 or /comments/1.json
   def destroy
+    authorize @comment
     @comment.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
@@ -64,13 +64,6 @@ class CommentsController < ApplicationController
     def set_comment
       @comment = Comment.find(params[:id])
     end
-
-    # def is_an_authorized_user
-    #   @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
-    #   if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
-    #     redirect_back fallback_location: root_url, alert: "Not authorized"
-    #   end
-    # end
 
     # Only allow a list of trusted parameters through.
     def comment_params
